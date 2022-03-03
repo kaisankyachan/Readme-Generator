c105113153372f63d921ed179d5b5023bd598483
kaiWhitney_ <kaisankyachan@gmail.com>
2022-03-02 21:31:25 -0700

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+const questions = ["What is the title of your project?", "Enter a description of your project.",
+     "Enter installation instructions.", "Enter usage information.","Enter contribution guidelines.",
+     "Enter test instructions.", "Choose a license for your application.",
+     "Enter your GitHub username.", "Enter your email."];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
